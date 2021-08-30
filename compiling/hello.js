26507c390c4cc3eb3a0961aa55a6848e5117374c
mjshuff23 <mjshuff23@gmail.com>
2021-08-30 15:36:41 -0400

    added tsc to README

--- compiling/hello.js
+++ compiling/hello.js
@@ -1,15 +1,11 @@
 // Yeee
 console.log("Hello, world!");
-
 // // This is an industrial-grade general-purpose greeter function
 // function greet(person, date) {
 //   console.log(`Hello ${person}! Today is ${date}`);
 // }
-
 // greet("John");
-
-
-function greet(user: string, date: Date) {
-  console.log(`Hello ${user}, today is ${date.toDateString()}!`);
+function greet(user, date) {
+    console.log(`Hello ${user}, today is ${date.toDateString()}!`);
 }
 greet("Michael", new Date());
