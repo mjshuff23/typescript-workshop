499749130e31e732abb3d45e935a460f4cb78217
Michael Shuff <mjshuff23@gmail.com>
2021-09-24 10:53:46 -0400

    finished type guards

--- workshop-3/04-type-guards-and-narrowing.ts
+++ workshop-3/04-type-guards-and-narrowing.ts
@@ -62,7 +62,34 @@ if (
 }
 
 // Obviously this is very messy, maybe we could turn the check into a function?
-function isCarLike(maybeCar: any) {
+// function isCarLike(maybeCar: any) {
+// return (
+//   maybeCar &&
+//   typeof maybeCar === 'object' &&
+//   'make' in maybeCar &&
+//   typeof maybeCar['make'] === 'string' &&
+//   'model' in maybeCar &&
+//   typeof maybeCar['model'] === 'string' &&
+//   'year' in maybeCar &&
+//   typeof maybeCar['year'] === 'number'
+// );
+// }
+
+// if (isCarLike(maybeCar)) {
+//   maybeCar;
+// }
+
+// As things stand right now, TypeScript seems to have no idea that the return value of
+//  isCarLike has anything to do with the type of maybeCar
+
+///////////////////////////////////// value is Foo /////////////////////////////////////
+/*
+  The first kind of user-defined type guard we will review is an is type guard. It is perfectly
+   suited for our example above because it’s meant to work in cooperation with a control flow
+   statement of some sort, to indicate that different branches of the “flow” will be taken based
+   on an evaluation of valueToTest’s type. Pay very close attention to isCarLike’s return type
+*/
+function isCarLike(maybeCar: any): maybeCar is CarLike {
   return (
     maybeCar &&
     typeof maybeCar === 'object' &&
@@ -79,5 +106,46 @@ if (isCarLike(maybeCar)) {
   maybeCar;
 }
 
-// As things stand right now, TypeScript seems to have no idea that the return value of
-//  isCarLike has anything to do with the type of maybeCar
+///////////////////////////////// asserts value is Foo /////////////////////////////////
+/*
+  There is another approach we could take that eliminates the need for a conditional.
+   Pay very close attention to assertsIsCarLike’s return type:
+*/
+
+function assertsIsCarLike(maybeCar: any): asserts maybeCar is CarLike {
+  if (
+    !(
+      maybeCar &&
+      typeof maybeCar === 'object' &&
+      'make' in maybeCar &&
+      typeof maybeCar['make'] === 'string' &&
+      'model' in maybeCar &&
+      typeof maybeCar['model'] === 'string' &&
+      'year' in maybeCar &&
+      typeof maybeCar['year'] === 'number'
+    )
+  ) {
+    throw new Error(`Value does not appear to be a CarLike${maybeCar}`);
+  }
+}
+
+maybeCar;
+assertsIsCarLike(maybeCar);
+maybeCar;
+
+//////////////////////////////// Writing High-Quality Guards ////////////////////////////////
+// It's important to remember not to make common mistakes when writing type guards.  For example,
+//  forgetting that 0 and "" will both evaluate to false here:
+// BAD EXAMPLE //
+function isNull(val: any): val is null {
+  return !val;
+}
+
+const empty = '';
+const zero = 0;
+if (isNull(zero)) {
+  console.log(zero);
+}
+if (isNull(empty)) {
+  console.log(empty);
+}
